b0604f8c28ba84a8bb0ce2ddc750eeef620c366a
lpitao <your-email-address>
2023-08-16 11:03:46 +0800

    注释jest代码

--- src/components/Button.test.ts
+++ src/components/Button.test.ts
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Button from '@/Button'; // 这里假设你设置了moduleNameMapper别名
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import Button from '@/Button'; // 这里假设你设置了moduleNameMapper别名
 
-test('renders button text', () => {
-  render(<Button text="Click me" />);
-  const buttonElement = screen.getByText(/Click me/i);
-  expect(buttonElement).toBeInTheDocument();
-});
+// test('renders button text', () => {
+//   render(<Button text="Click me" />);
+//   const buttonElement = screen.getByText(/Click me/i);
+//   expect(buttonElement).toBeInTheDocument();
+// });
